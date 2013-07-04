1e4d461f70aa80b71ab691655b18a04fcd8f031c
Muhammad Yasir Zahoornpearls <yasir@yasir-Lenovo-G580.(none)>
2013-07-04 16:00:34 +0500

    Initial import

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,49 @@
+class BookController < ApplicationController
+   def list
+	@books = Book.find(:all)
+   end
+   def show
+	@book = Book.find(params[:id])
+   end
+   def new
+	@book = Book.new
+      	@subjects = Subject.find(:all)
+   end
+   def create
+
+
+#@book = Book.new(params[:book])
+#abort(@book);
+
+	unless params[:book].blank?
+  		@book = Book.new(params[:book].permit(:title, :price, :subject_id, :description))
+  		#@book.save
+	      	if @book.save
+        	    redirect_to :action => 'list'
+      		else
+            	    @subjects = Subject.all
+            	    render :action => 'new'
+      		end
+	end    
+   end
+   def edit
+      	@book = Book.find(params[:id])
+      	@subjects = Subject.find(:all)
+   end
+   def update
+	@book = Book.find(params[:id])
+      	if @book.update_attributes(params[:book].permit(:title, :price, :subject_id, :description))
+         redirect_to :action => 'show', :id => @book
+      	else
+         @subjects = Subject.find(:all)
+         render :action => 'edit'
+      	end
+   end
+   def delete
+	Book.find(params[:id]).destroy
+      	redirect_to :action => 'list'
+   end
+   def show_subjects
+      @subjects = Subject.find(params[:id])
+   end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Project3::Application.config.secret_key_base = '701b75f9b235f35c5fa9cd1b15b2844683ae3bc66aab586ac0f4050b6c30a639202484060575e1b6059d3bd7c98ff9b42066c5861e35021083f2619ed16ceafa'
